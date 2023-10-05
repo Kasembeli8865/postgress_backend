344781776cd9e4159f183280cdbdc1ba88bae768
Kasembeli8865 <lionel.kasembeli@student.moringaschool.com>
2023-10-05 14:20:59 +0300

    Added CORS

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ from routes import main
 from routes import hospital_routes
 from seed import seed_data
 import os
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
